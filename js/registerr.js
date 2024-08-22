99cbf2e7659abdf34185d3075b3e55ad7133c2f6
soponta <heber.sba@gmail.com>
2024-08-22 22:44:40 +0700

    Delete js/registerr.js

--- js/registerr.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded',init,false);function init(){if('serviceWorker'in navigator&&navigator.onLine){navigator.serviceWorker.register('/service-worker.js').then((reg)=>{console.log('Registrasi service worker Berhasil',reg);},(err)=>{console.error('Registrasi service worker Gagal',err);});}}
-let installPrompt=null;const installButton=document.querySelector("#install");window.addEventListener("beforeinstallprompt",(event)=>{event.preventDefault();installPrompt=event;installButton.removeAttribute("hidden");});function disableInAppInstallPrompt(){installPrompt=null;}
-function disableInAppInstallPrompt(){installPrompt=null;}
-window.addEventListener("appinstalled",()=>{disableInAppInstallPrompt();});
